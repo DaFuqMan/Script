29d6bcb83b33dfb90e2686e0605784deec8ece65
DaFuqMan <40741082+DaFuqMan@users.noreply.github.com>
2018-07-04 09:08:54 +0530

    Create Source.sh

---
+++ Source.sh
@@ -0,0 +1,8 @@
+rm -rf packages/apps/Settings
+git clone https://github.com/DaFuqMan/android_packages_apps_Settings -b o8x packages/apps/Settings
+
+rm -rf frameworks/base
+git clone https://github.com/DaFuqMan/android_frameworks_base -b o8x frameworks/base
+
+rm -rf vendor/du
+git clone https://github.com/DaFuqMan/android_vendor_du -b o8x vendor/du
